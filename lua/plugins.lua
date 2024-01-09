692976e9af6a8743da6668cbd609cbd18e72778d
ingwar1991 <ingwar1991@gmail.com>
2024-01-09 20:12:33 +0200

    added dadbod

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -75,4 +75,23 @@ return {
 
 --	Xdebug  
     'vim-vdebug/vdebug',
+
+--  Database
+    {
+        'kristijanhusak/vim-dadbod-ui',
+        dependencies = {
+            { 'tpope/vim-dadbod', lazy = true },
+            { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+        },
+        cmd = {
+            'DBUI',
+            'DBUIToggle',
+            'DBUIAddConnection',
+            'DBUIFindBuffer',
+        },
+        init = function()
+            -- Your DBUI configuration
+            vim.g.db_ui_use_nerd_fonts = 1
+        end,
+    }
 }
